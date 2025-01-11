8204ebc5b9b0d69a81ff23783204b30198fa2458
Franz Blömer <61627082+FlachbandkabelPeter@users.noreply.github.com>
2025-01-11 14:39:23 +0100

    Update main.go

--- server/main.go
+++ server/main.go
@@ -1,1 +1,47 @@
+package main
 
+import (
+    "fmt"
+    "log"
+    "net/http"
+
+    "github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+    CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println("Upgrade error:", err)
+        return
+    }
+    defer conn.Close()
+
+    for {
+        // Einfaches Echo
+        messageType, msg, err := conn.ReadMessage()
+        if err != nil {
+            log.Println("Read error:", err)
+            break
+        }
+        log.Printf("Got message: %s", msg)
+
+        // Echo zurückschicken
+        if err = conn.WriteMessage(messageType, msg); err != nil {
+            log.Println("Write error:", err)
+            break
+        }
+    }
+}
+
+func main() {
+    http.HandleFunc("/ws", wsHandler)
+
+    fmt.Println("Starting server at :8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal("ListenAndServe:", err)
+    }
+}
